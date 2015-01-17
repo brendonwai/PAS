66b975e0640cef9d3b3921ca0a094f6a0127dc8e
DK <twosdae@hotmail.com>
2015-01-17 13:20:32 -0800

    SpawnTest

---
+++ Assets/Resources/Scripts/Instantiate Objects.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantiateObjects : MonoBehaviour {
+
+	public GameObject GameObject = null;
+	// Use this for initialization
+	void Start () {
+		GameObject = Instantiate(Resources.Load("Prefabs/Cube")) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
