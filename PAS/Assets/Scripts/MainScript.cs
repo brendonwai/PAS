8669c6f492523bb561e4244a3358bd4d58b6ec2b
Winston Liang <winstoyl@uci.edu>
2015-01-17 19:31:23 -0800

    Changes to Main and Timer

--- PAS/Assets/Scripts/MainScript.cs
+++ PAS/Assets/Scripts/MainScript.cs
@@ -1,49 +1,106 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MainScript : MonoBehaviour {
 	QuestionGenerator QG = new QuestionGenerator();
-	GameObject levelobj;
 	ScoreScript score;
-	public int level = 1;
-	GameObject timerobj;
+	public int level;
+	public int lives;
 	TimerScript timersc;
 	int time;
-
+	Light light;
+	int state;
+	int choice;
+	Text livestext;
+	
 	// Use this for initialization
 	void Start () {
-		levelobj = GameObject.Find ("LevelText");
-		score = levelobj.GetComponent <ScoreScript> ();
-		timerobj = GameObject.Find ("TimerText");
-		timersc = timerobj.GetComponent <TimerScript> ();
-		time = timersc.count;
+		light = GameObject.Find ("Directional light").GetComponent<Light>();
+		score = GameObject.Find ("LevelText").GetComponent <ScoreScript> ();
+		timersc = GameObject.Find ("TimerText").GetComponent <TimerScript> ();
+		//livestext = GameObject.Find ("LivesText").GetComponent<Text> ();
+		time = timersc.count;;
+		level = 1;
+		//		lives = 3;
+		//livestext.text = "";//lives.ToString ();
+		state = 0;
+		
+	}
+	
+	void dimLight(){
+		if (light.intensity < 5) {
+			light.intensity += 0.1f;
+		}
+	}
+	
+	void litLight(){
+		if (light.intensity > 0.5) {
+			light.intensity -= 0.25f;
+		}
+	}
+	
+	public void LeftButton(){
+		choice =  0;
+	}
+	
+	public void RightButton(){
+		choice =  1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		question ();
-		displayBlockFall ();
-		guess ();
-	}
-
+		if (state == 0) {
+			question ();
+		}
+		if (state == 1) {
+			displayBlockFall ();
+		}
+		if (state == 2) {
+			guess ();
+		}
+	}	
+	
 	void question(){
-		Question newQuestion = QG.getQuestion(level);
-		Debug.Log (newQuestion.color.ToString ());
-		}
-
+		Question newQuestion = QG.getQuestion(1);
+		string question;
+		if (newQuestion.color == null && newQuestion.shape == null)
+			question = "Which side has " + newQuestion.quantity + " objects?";
+		else if (newQuestion.color == null && newQuestion.shape != null) 
+			question = "Which side has " + newQuestion.quantity + " " + newQuestion.shape + "s?";
+		else if (newQuestion.shape == null && newQuestion.color != null)
+			question = "Which side has " + newQuestion.quantity + " " + newQuestion.color + "s?";
+		else 
+			question = "Which side has " + newQuestion.quantity + " " + newQuestion.color + " " + newQuestion.shape + "s?";
+		state = 1;
+	}
+	
 	void displayBlockFall(){
-		}
-
+		//litLight ();
+		dimLight ();
+		state = 2;
+	}
+	
 	void guess(){
 		timersc.StartTimer ();
+		Debug.Log ("dolan");
 		time = timersc.count;
 		if (time > 0) {
-				}
-		else {
-			Application.LoadLevel ("GameOver");
-			level += 1;
-			score.up ();
+			if (choice > -1){
+				score.up ();
+				//choice is passed here
 			}
 		}
-
+		else {
+			GameOver ();
+		}
+		state = 0;
+	}
+	
+	void GameOver(){
+		Application.LoadLevel ("GameOver");
+	}
+	
 }
+
+
