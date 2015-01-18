b46f8d034480346fec26f37e615120f6c45d3fbe
Winston Liang <winstoyl@uci.edu>
2015-01-17 16:08:55 -0800

    Added Timer and Score texts, a test level for the main function with a game over level

--- PAS/Assets/Scripts/Question.cs
+++ PAS/Assets/Scripts/Question.cs
@@ -13,7 +13,7 @@ public class QuestionGenerator{
 	private static int[] colors = {0,1,2,3,4,5};
 	private static int[] shapes = {0,1,2,3};
 
-	Question createQuestion(Question question, int shape, int color, int quantity, double ratio){
+	public Question createQuestion(Question question, int shape, int color, int quantity, double ratio){
 		question.shape = shape;
 		question.color = color;
 		question.quantity = quantity;
@@ -22,7 +22,7 @@ public class QuestionGenerator{
 	}
 	// Returns the question and the win parameters, given some number of function parameters based on the difficulty.
 	// The returns a question's indexs for a predefined array in the form <question, color, shape, side,ratio>.
-	Question getQuestion(int level) {
+	public Question getQuestion(int level) {
 		System.Random r = new System.Random();
 		int randomColor = 0;
 		int randomShape = 0;
