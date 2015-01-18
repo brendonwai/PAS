9fd3086979db67fd6c1769f3c6a0cb94943d6f70
Winston Liang <winstoyl@uci.edu>
2015-01-18 14:28:57 -0800

    looktime variable added

--- PAS/Assets/Scripts/MainScript.cs
+++ PAS/Assets/Scripts/MainScript.cs
@@ -22,6 +22,8 @@ public class MainScript : MonoBehaviour {
 	bool choosing;
 	Instantiate generator;
 	bool more;
+	bool wonnered;
+	public float lookTime;
 	
 	// Use this for initialization
 	void Start () {
@@ -66,7 +68,9 @@ public class MainScript : MonoBehaviour {
 	void nextLevel(){
 		level += 1;
 		lvl.uplvl ();
-		lvl.upscore (5);
+		if (wonnered == true) {
+			lvl.upscore (5);
+		}
 		state = 0;
 		LB.changestate ();
 		RB.changestate ();
@@ -117,7 +121,7 @@ public class MainScript : MonoBehaviour {
 			falling = true;
 			timer0 = Time.time;
 		}
-		if (Time.time - timer0 > 3){
+		if (Time.time - timer0 > lookTime){
 			falling = false;
 			state = 2;
 		}
