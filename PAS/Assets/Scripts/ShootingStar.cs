d869753f0c7093c1a9318215521f7b4f3d533ada
SVT125 <Jhardjadinata14@gmail.com>
2015-01-18 15:15:20 -0800

    readded star

--- PAS/Assets/Scripts/ShootingStar.cs
+++ PAS/Assets/Scripts/ShootingStar.cs
@@ -3,18 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class ShootingStar : MonoBehaviour {
-	public float xSpeed, ySpeed, amplitude;
 	public GameObject shootingStar;
-	private Vector3 currentPosition;
 	System.Random r = new System.Random();
-	public float randomScalar;
+	//public float randomScalar;
+	public float lastForce;
 	void Start() {
-		//GameObject star = new GameObject ("ShootingStar");
-		randomScalar = r.Next (10)/100;
+		//randomScalar = r.Next (10)/100;
 		shootingStar = Instantiate (shootingStar, transform.position, Quaternion.identity) as GameObject;
 		shootingStar.rigidbody.AddForce (Vector3.right * 400);
 	}
 	void Update() {
-		shootingStar.rigidbody.AddForce(Vector3.up * Mathf.Sin (Time.realtimeSinceStartup) );
+		shootingStar.rigidbody2D.velocity = new Vector2 (1, 2).normalized * 20;
+		float argument = Time.realtimeSinceStartup * 100;
+		shootingStar.rigidbody.AddForce(Vector3.up * (Mathf.Sin (argument) * 500 - (2*lastForce)));
+		lastForce = Mathf.Sin (argument) * 500;
 	}
 }
