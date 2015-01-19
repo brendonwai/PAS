76e5689e5c75d8570e912e86d08c9a9eba536371
Connor Stokes <stokesc@uci.edu>
2015-01-18 23:28:04 -0800

    Made Main Menu badass

---
+++ PAS/Assets/Scripts/BlocksTooHigh.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlocksTooHigh : MonoBehaviour {
+
+	public BoxCollider2D blockStopper;
+
+	public float removeStopperDuration;
+
+	float removeStopperTimestamp;
+
+	bool triggerIsActive = false; 
+
+	void Start()
+	{
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Shape")
+		{
+			Debug.Log ("HEYENTER");
+			blockStopper.isTrigger = true;
+			triggerIsActive = true;
+			removeStopperTimestamp = removeStopperDuration + Time.time;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.tag == "Shape")
+		{
+			Debug.Log ("HEYSTAY");
+			blockStopper.isTrigger = true;
+			triggerIsActive = true;
+			removeStopperTimestamp = removeStopperDuration + Time.time;
+		}
+	}
+
+
+	void Update()
+	{
+		Debug.Log (triggerIsActive);
+		if (Time.time > removeStopperTimestamp && triggerIsActive == true)
+		{
+			Debug.Log("yep");
+			blockStopper.isTrigger = false;
+			triggerIsActive = false;
+		}
+
+	}
+}
