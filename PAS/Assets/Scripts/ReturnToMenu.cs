660db4906e8ff97f242d72e4ccf9838d1cf2fe53
jdalpert <kaylab@dhcp-v077-018.mobile.uci.edu>
2015-01-18 22:12:31 -0800

    Sounds

--- PAS/Assets/Scripts/ReturnToMenu.cs
+++ PAS/Assets/Scripts/ReturnToMenu.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class ReturnToMenu : MonoBehaviour {
 
 	// Use this for initialization
+	public void wait(){
+		InvokeRepeating ("returnToMenu", 0.5f, 0);
+	}
 	public void returnToMenu()
 	{
 		Application.LoadLevel("MAINMENU");
