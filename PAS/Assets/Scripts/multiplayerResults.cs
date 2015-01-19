0993c6b58c59e7cebd4d753fff00f2bab467f696
Connor Stokes <stokesc@uci.edu>
2015-01-18 19:49:56 -0800

    Added MP Victory Screen

---
+++ PAS/Assets/Scripts/multiplayerResults.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class multiplayerResults : MonoBehaviour {
+
+	public string winner = "GAME OVER";
+
+	// Use this for initialization
+	void Start () {
+
+		Object.DontDestroyOnLoad(this.gameObject);
+	}
+
+	
+	public void setWinner(string playerName)
+	{
+		winner = playerName + "Wins";	
+	}
+
+}
