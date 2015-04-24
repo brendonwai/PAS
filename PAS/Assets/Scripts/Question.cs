6db083d60b0343c55e0c6af864cd63dcc41867d9
SVT125 <Jhardjadinata14@gmail.com>
2015-04-23 17:59:54 -0700

    fixed ratio syntax

--- PAS/Assets/Scripts/Question.cs
+++ PAS/Assets/Scripts/Question.cs
@@ -27,10 +27,10 @@ public class QuestionGenerator{
 		string randomColor = null;
 		string randomShape = null;
 		string randomQuantity;
-		//double ratio = .55-(1/(1+e^((x+50)/50)));		
+		double ratio = .55-(1/(1+Math.Pow(Math.E,(level+50)/50)));		
 		randomQuantity = quantities [r.Next (quantities.Length)];
 		randomShape = shapes [r.Next (shapes.Length)];
 		randomColor = colors [r.Next (colors.Length)]; 
-		return createQuestion (new Question (), randomShape, randomColor, randomQuantity, 1);
+		return createQuestion (new Question (), randomShape, randomColor, randomQuantity, ratio);
 	} 
 }
