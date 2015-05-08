d294203f655dfcf34059dd67e8d590674a26c81c
jdalpert <josh1994bv@gmail.com>
2015-05-08 14:52:33 -0700

    Changes to the main script
    fixed, more efficient, reduced 100 lines, actually readable.

--- PAS/Assets/Scripts/MainScript.cs
+++ PAS/Assets/Scripts/MainScript.cs
@@ -6,8 +6,6 @@ public class MainScript : MonoBehaviour {
 	QuestionGenerator QG = new QuestionGenerator();
 	ScoreScript lvl;
 	public int level;
-	int lives;
-	int lives2;
 	bool p1;
 	bool c1;
 	bool p2;
@@ -16,9 +14,9 @@ public class MainScript : MonoBehaviour {
 	int time;
 	Light lite;
 	int state;
-	int choice;
+	int choice1;
 	int choice2;
-	Text q;
+	Text shownQuestion;
 	string lvlquestion;
 	DisableButton LB;
 	DisableButton RB;
@@ -29,14 +27,17 @@ public class MainScript : MonoBehaviour {
 	Instantiate generator;
 	bool more;
 	public float looktime;
-	Text livestext;
 	multiplayerResults result;
 	Play sound;
 
 	// Use this for initialization
 	void Start () {
-		choice = -1;
+
+		//choice checks answers 
+		choice1 = -1;
 		choice2 = -1;
+
+		//grabs all the objects, and sets them to variables
 		sound = GameObject.Find ("SoundPlayer").GetComponent<Play>();
 		result = GameObject.Find ("multiplayerResults").GetComponent<multiplayerResults> ();
 		generator = GameObject.Find ("Generators").GetComponent<Instantiate>();
@@ -45,12 +46,13 @@ public class MainScript : MonoBehaviour {
 		lite = GameObject.Find ("Directional light").GetComponent<Light>();
 		lvl = GameObject.Find ("LevelText").GetComponent <ScoreScript> ();
 		timersc = GameObject.Find ("TimerText").GetComponent <TimerScript> ();
-		q = GameObject.Find ("QuestionText").GetComponent <Text> ();
-		q.text = "";
+		shownQuestion = GameObject.Find ("QuestionText").GetComponent <Text> ();
+		shownQuestion.text = "";
+
 		time = timersc.count;
 		level = 1;
 		state = 0;
-		looktime = 5f;
+		looktime = 10f;
 	}
 	
 	void dimLite(){
@@ -64,78 +66,106 @@ public class MainScript : MonoBehaviour {
 			GetComponent<Light>().intensity -= 0.25f;
 		}
 	}
-	
-//	public void LeftButton(){
-//		choice =  0;
-//	}
-//	
-//	public void RightButton(){
-//		choice =  1;
-//	}
 
 	void nextLevel(bool correct1, bool correct2){
-		level += 1;
-		lvl.uplvl ();
-		if (correct1)
-			lvl.upscore (5);
-		if (correct2)
-			lvl.upscore2 (5);
-		if (level >= 10 && lvl.score != lvl.score2)
-			GameOver ();
-		c1 = false;
-		c2 = false;
-		p1 = false;
-		p2 = false;
-		choice = -1;
-		choice2 = -1;
-		state = 0;
-		LB.changestate ();
-		RB.changestate ();
-		q.text = "";
-		choosing = false;
-		timersc.started = false;
-		generator.clearShapes ();
+		//increases level and level difficulty 
+		increaseScore (correct1, correct2);
+		clearScreen ();
+
+		//reduce look timer and to increase difficulty
 		if (looktime > 3) {
 			looktime -= 0.1f;
-			}
+		}
 	}
-	
+
+	void increaseScore(bool player1Correct, bool player2Correct){
+		//increases level and level difficulty 
+		level += 1;
+		lvl.uplvl ();
+		if (player1Correct)
+			lvl.upscore (5);
+		if (player2Correct)
+			lvl.upscore2 (5);
+		if (level >= 10 && lvl.score != lvl.score2)
+			GameOver ();                                         
+	}
+
+	void clearScreen(){
+		clearPlayerInfo ();
+		//state to tell what the game needs to do next
+		state = 0;
+		//cleans up buttons
+		LB.changestate ();
+		RB.changestate ();
+		//clears question
+		shownQuestion.text = "";
+		//allow no one to choose answer
+		choosing = false;
+		//turn off timer
+		timersc.started = false;
+		//clear blocks on the screen for restart
+		generator.clearShapes ();
+	}
+
+	void clearPlayerInfo(){
+		//valid players
+		p1 = false;
+		p2 = false;
+		//boolean to hold if the player choices correctly 
+		c1 = false;
+		c2 = false;
+		//int to determine the choice 
+		choice1 = -1;
+		choice2 = -1;
+	}
+
 	// Update is called once per frame
 	void Update () {
-		if (state == 0) {
+		switch(state){
+			//create question
+		case 0:
 			question ();
-		}
-		if (state == 1) {
+			break;
+			//create blocks
+		case 1:
 			displayBlockFall ();
-		}
-		if (state == 2) {
+			break;
+			//answer question
+		case 2:
 			guess ();
+			break;
 		}
 	}	
-	
+	//makes the question fo the game
 	void question(){
 		Question newQuestion = QG.getQuestion(level);
-		if (newQuestion.quantity == "more") {
+		checkMoreLess (newQuestion);
+		generateQuestion (newQuestion);
+	}
+	//checks More or Less for the question
+	void checkMoreLess (Question currentQ){
+		if (currentQ.quantity == "more")
 			more = true;
-		} 
-		else {
-			more = false;
-		}
-
-
-		if (newQuestion.color == null && newQuestion.shape == null)
-			lvlquestion = "Which side has " + newQuestion.quantity + " objects?";
-		else if (newQuestion.color == null && newQuestion.shape != null) 
-			lvlquestion = "Which side has " + newQuestion.quantity + " " + newQuestion.shape + "s?";
-		else if (newQuestion.shape == null && newQuestion.color != null)
-			lvlquestion = "Which side has " + newQuestion.quantity + " " + newQuestion.color + "s?";
 		else 
-			lvlquestion = "Which side has " + newQuestion.quantity + " " + newQuestion.color + " " + newQuestion.shape + "s?";
-		string temp =newQuestion.creationRatio.ToString();
-		tally.SendMessage ("Load",new string[]{temp, newQuestion.color, newQuestion.shape});
+			more = false;
+	}
+
+	//Outs question and stores it;
+	void generateQuestion (Question currentQ){
+		if (currentQ.color == null && currentQ.shape == null)
+			lvlquestion = "Which side has " + currentQ.quantity + " objects?";
+		else if (currentQ.color == null && currentQ.shape != null) 
+			lvlquestion = "Which side has " + currentQ.quantity + " " + currentQ.shape + "s?";
+		else if (currentQ.shape == null && currentQ.color != null)
+			lvlquestion = "Which side has " + currentQ.quantity + " " + currentQ.color + "s?";
+		else 
+			lvlquestion = "Which side has " + currentQ.quantity + " " + currentQ.color + " " + currentQ.shape + "s?";
+		string temp = currentQ.creationRatio.ToString();
+		tally.SendMessage ("Load",new string[]{temp, currentQ.color, currentQ.shape});
 		state = 1;
 	}
-	
+
+	//shows falling blocks to the screen
 	void displayBlockFall(){
 		if (falling == false) {
 			falling = true;
@@ -145,102 +175,54 @@ public class MainScript : MonoBehaviour {
 			falling = false;
 			state = 2;
 		}
-		//litLite ();
-		//dimLite ();
+
 	}
 	
 	void guess(){
+		//assigns input to keys
 		if (Input.GetKey("a") && p1 == false)
-			choice = 0;
+			choice1 = 0;
 		if (Input.GetKey("d") && p1 == false)
-			choice = 1;
+			choice1 = 1;
 		if (Input.GetKey("left") && p2 == false)
 			choice2 = 0;
 		if (Input.GetKey("right") && p2 == false)
 			choice2 = 1;
-		
+
+		//flips to turn on buttons
 		if (choosing == false) {
 			choosing = true;
 			LB.changestate ();
 			RB.changestate ();
-				}
+		}
+
+		//outputs the question to the screen
+		startQuestion ();
+	}
+	void startQuestion (){
+		//starts timer and outputs the question similtaneously 
 		lvl.lvlText.text = "";
-		q.text = lvlquestion;
+		shownQuestion.text = lvlquestion;
 		timersc.StartTimer ();
 		time = timersc.count;
+		CheckTimer (time);
+	}
+	//Checks to see if the game is over 
+	void GameOver(){
+		if (lvl.score > lvl.score2)
+			result.setWinner ("Player 1 Wins");
+		else
+			result.setWinner ("Player 2 Wins");
+		Application.LoadLevel ("GameOver");
+	}
+
+	void CheckTimer(int time){
 		if (time > 0) {
-						if (choice > -1 && p1 == false) {
-				sound.Song ();
-								if (tally.GetComponent<ObjectTally> ().LeftMore == true) {
-										if (more == true) {
-												if (choice == 0) {
-														c1 = true;
-							sound.Song ();
-												} else {
-														c1 = false;
-												}
-										} else {
-												if (choice == 1) {
-														c1 = true;
-												} else {
-														c1 = false;
-												}
-										}
-								} else {
-										if (more == true) {
-												if (choice == 1) {
-														c1 = true;
-												} else {
-														c1 = false;
-												}
-										} else {
-												if (choice == 0) {
-														c1 = true;
-												} else {
-														c1 = false;
-												}
-										}
-								}
-								p1 = true;
-								choice = -1;
-						}
-						//player2
-						if (choice2 > -1 && p2 == false) {
-				sound.Song ();
-								if (tally.GetComponent<ObjectTally> ().LeftMore == true) {
-										if (more == true) {
-												if (choice2 == 0) {
-														c2 = true;
-												} else {
-														c2 = false;
-												}
-										} else {
-												if (choice2 == 1) {
-														c2 = true;
-												} else {
-														c2 = false;
-												}
-										}
-								} else {
-										if (more == true) {
-												if (choice2 == 1) {
-														c2 = true;
-												} else {
-														c2 = false;
-												}
-										} else {
-												if (choice2 == 0) {
-														c2 = true;
-												} else {
-														c2 = false;
-												}
-										}
-								}
-				p2 = true;
-								choice2 = -1;
-						}
-				}
-		else {
+			//player1
+			checkPlayerChoice (ref choice1, ref p1, ref c1);
+			//player2
+			checkPlayerChoice (ref choice2, ref p2, ref c2);
+		}else{
 			if (level >= 10 && lvl.score != lvl.score2)
 				GameOver ();
 			else{
@@ -248,15 +230,31 @@ public class MainScript : MonoBehaviour {
 			}
 		}
 	}
-	
-	void GameOver(){
-		if (lvl.score > lvl.score2)
-						result.setWinner ("Player 1 Wins");
+	void checkPlayerChoice(ref int choice, ref bool player, ref bool validChoice){
+		if (choice > -1 && player == false) {
+			sound.Song ();
+			if (tally.GetComponent<ObjectTally> ().LeftMore == true) {
+				if (more == true) 
+					flipChoice(ref choice, ref validChoice, 0);
 				else
-						result.setWinner ("Player 2 Wins");
-		Application.LoadLevel ("GameOver");
+					flipChoice(ref choice, ref validChoice, 1);
+			} else {
+				if (more == true)
+					flipChoice(ref choice, ref validChoice, 1);
+				else 
+					flipChoice(ref choice, ref validChoice, 0);
+			}
+			player = true;
+			choice = -1;
+		}
 	}
-	
+
+	void flipChoice(ref int choice, ref bool validChoice, int cmp){
+		if (choice == cmp)
+			validChoice = true;
+		else
+			validChoice = false;
+	}
 }
 
 
