1df91574f3d785eca79796bd25b711ba2b7f20f6
Connor Stokes <stokesc@uci.edu>
2015-05-09 01:21:19 -0700

    Fixed shooting star

--- PAS/Assets/Scripts/ShootingStar.cs
+++ PAS/Assets/Scripts/ShootingStar.cs
@@ -4,10 +4,13 @@ using System.Collections.Generic;
 
 public class ShootingStar : MonoBehaviour {
 	public GameObject shootingStar;
+    GameObject cloneStar;
 	System.Random r = new System.Random();
 	public float lastForce;
 	bool isUp = true;
 	int counter = 0; //To simulate adding force every n number of seconds.
+
+    bool starAlive = false;
 
 	void Start() {
 		
@@ -16,20 +19,21 @@ public class ShootingStar : MonoBehaviour {
 	void Update() {
 		if (Time.time % .5 < .1)
 			counter++;
-		if(counter % 5 == 0) {
+		if(counter % 5 == 0 && starAlive) {
 			//Random movement simulation
 			if(isUp)
-				shootingStar.GetComponent<Rigidbody2D>().AddForce (Vector3.up * 300);
+				cloneStar.GetComponent<Rigidbody2D>().AddForce (Vector3.up * 300);
 			else
-				shootingStar.GetComponent<Rigidbody2D>().AddForce (Vector3.down * 300);
+				cloneStar.GetComponent<Rigidbody2D>().AddForce (Vector3.down * 300);
 			isUp = !isUp;
 		}
 	}
 	public void starFactory() {
-		shootingStar = Instantiate (shootingStar,new Vector3(-15,(float)(r.NextDouble () * 5 - 5),0), Quaternion.identity) as GameObject;
-		shootingStar.GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = "star";
-		shootingStar.GetComponent<Rigidbody2D>().AddForce (Vector3.right * 400);
-		shootingStar.GetComponent<Rigidbody2D>().AddForce (Vector3.up * 100);
+		cloneStar = Instantiate (shootingStar,new Vector3(-15,(float)(r.NextDouble () * 5 - 5),0), Quaternion.identity) as GameObject;
+        starAlive = true;
+		cloneStar.GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = "star";
+		cloneStar.GetComponent<Rigidbody2D>().AddForce (Vector3.right * 400);
+		cloneStar.GetComponent<Rigidbody2D>().AddForce (Vector3.up * 100);
 	}
 	public Vector3 destroyStar() {
 		Vector3 lastPos = new Vector3 ();
@@ -37,6 +41,7 @@ public class ShootingStar : MonoBehaviour {
 		{
 			lastPos = shape.transform.position;
 			Object.Destroy(shape);
+            starAlive = false;
 		}
 		return lastPos;
 	}
