576aa8fb7616396db627fe0e327f9a60c6144816
SVT125 <Jhardjadinata14@gmail.com>
2015-05-16 12:08:36 -0700

    resetting ratio after every threshold, haven't tested

--- PAS/Assets/Scripts/Question.cs
+++ PAS/Assets/Scripts/Question.cs
@@ -14,6 +14,7 @@ public class QuestionGenerator{
 	private static string[] quantities = {"more","fewer"};
 	private static string[] colors = {"blue","red","green","purple","yellow"};
 	private static string[] shapes = {"circle","square","triangle"};
+	public int[] thresholds = {5,12,22,40}; //artificial thresholds to add in shapes/colors/adjust ratio
 
 	public Question createQuestion(Question question, string shape, string color, string quantity, double ratio, int level){
 		question.shape = shape;
@@ -30,23 +31,34 @@ public class QuestionGenerator{
 	public Question getQuestion(int level) {
 		System.Random r = new System.Random();
 		string randomColor = null, randomShape = null, randomQuantity = null;
-		double ratio = (.55-(1/(1+Math.Pow(Math.E,(level+50)/50))));
 		randomQuantity = quantities [r.Next (quantities.Length)];
 		randomShape = shapes [r.Next (shapes.Length)];
 		randomColor = colors [r.Next (colors.Length)]; 
+
+		double ratio;
+		if(level < thresholds[0]) {
+			ratio = (.55-(1/(1+Math.Pow(Math.E,((level-thresholds[0])+50)/50))));
+		} else if (level < thresholds[1]) {
+			ratio = (.55-(1/(1+Math.Pow(Math.E,((level-thresholds[1])+50)/50))));
+		} else if(level < thresholds[2]) {
+			ratio = (.55-(1/(1+Math.Pow(Math.E,((level-thresholds[2])+50)/50))));
+		} else if(level < thresholds[3]) {
+			ratio = (.55-(1/(1+Math.Pow(Math.E,((level-thresholds[3])+50)/50))));
+		} else
+			ratio = (.55-(1/(1+Math.Pow(Math.E,(level+50)/50))));
 		return createQuestion (new Question (), randomShape, randomColor, randomQuantity, ratio, level);
 	} 
 
 	//Returns an array of size 2 = [number of colors, number of shapes] given the level. Color range of [2,5], shape range of [2,3].
 	private int[] levelParameters(int level) {
 		int[] parameters;
-		if(level < 5) {
+		if(level < thresholds[0]) {
 			parameters = new int[]{2,2};
-		} else if (level < 12) {
+		} else if (level < thresholds[1]) {
 			parameters = new int[]{3,2};
-		} else if(level < 22) {
+		} else if(level < thresholds[2]) {
 			parameters = new int[]{3,3};
-		} else if(level < 40) {
+		} else if(level < thresholds[3]) {
 			parameters = new int[]{4,3};
 		} else
 			parameters = new int[]{5,3};
