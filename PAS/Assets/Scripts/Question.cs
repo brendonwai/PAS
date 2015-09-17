5b79af489b40ae1b2ec8a08bdc53350e4cca9d78
SVT125 <Jhardjadinata14@gmail.com>
2015-09-17 13:46:10 -0700

    Shapes made optional in question generator

--- PAS/Assets/Scripts/Question.cs
+++ PAS/Assets/Scripts/Question.cs
@@ -14,9 +14,10 @@ public class QuestionGenerator{
 	private static string[] quantities = {"more","fewer"};
 	private static string[] colors = {"blue","red","green","purple","yellow"};
 	private static string[] shapes = {"circle","square","triangle"};
-	public int[] thresholds = {5,12,22,40}; //artificial thresholds to add in shapes/colors/adjust ratio
+	public int[] thresholds = {5,12,22,40}; // Artificial thresholds to add in shapes/colors/adjust ratio
+	public int shapeThreshold = 10; // The threshold at which questions will start asking different shape types 
 
-	public Question createQuestion(Question question, string shape, string color, string quantity, double ratio, int level){
+	private Question createQuestion(Question question, string shape, string color, string quantity, double ratio, int level){
 		question.shape = shape;
 		question.color = color;
 		question.quantity = quantity;
@@ -27,12 +28,11 @@ public class QuestionGenerator{
 		return question;
 	}
 	// Returns the question and the win parameters, given some number of function parameters based on the difficulty.
-	// The returns a question's indexs for a predefined array in the form <question, color, shape, side,ratio>.
+	// The returns a question's indexes for a predefined array in the form <question, color, shape, side,ratio>.
 	public Question getQuestion(int level) {
 		System.Random r = new System.Random();
 		string randomColor = null, randomShape = null, randomQuantity = null;
 		randomQuantity = quantities [r.Next (quantities.Length)];
-		randomShape = shapes [r.Next (shapes.Length)];
 		randomColor = colors [r.Next (colors.Length)]; 
 
 		double ratio;
@@ -46,7 +46,13 @@ public class QuestionGenerator{
 			ratio = (.55-(1/(1+Math.Pow(Math.E,((level-thresholds[3])+50)/50f))));
 		} else
 			ratio = (.55-(1/(1+Math.Pow(Math.E,(level+50)/50f))));
-		return createQuestion (new Question (), randomShape, randomColor, randomQuantity, ratio, level);
+
+		// If we have passed the shape level threshold, then discriminate shapes for the question - else, only colors.
+		if (level > shapeThreshold) {
+			randomShape = shapes [r.Next (shapes.Length)];
+			return createQuestion (new Question (), randomShape, randomColor, randomQuantity, ratio, level);
+		} else
+			return createQuestion (new Question (), randomShape, randomColor, randomQuantity, ratio, level);
 	} 
 
 	//Returns an array of size 2 = [number of colors, number of shapes] given the level. Color range of [2,5], shape range of [2,3].
